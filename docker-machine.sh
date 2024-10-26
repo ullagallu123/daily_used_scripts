fd24ade23b511ac9668fd7a3a782d5a14d8a2279
SIVARAMAKRISHNA <51481385+ullagallu123@users.noreply.github.com>
2024-10-26 20:04:20 +0530

    change the type one-time to persistent

--- docker-machine.sh
+++ docker-machine.sh
@@ -4,6 +4,6 @@ aws ec2 run-instances \
   --instance-type t3a.large \
   --key-name siva \
   --user-data file://docker-installation.sh \
-  --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=one-time}" \
+  --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent}" \
   --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=docker}]' \
   --block-device-mappings 'DeviceName=/dev/xvda,Ebs={VolumeSize=50,VolumeType=gp3,DeleteOnTermination=true}'
