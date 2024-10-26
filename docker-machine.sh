0e36c9ce010863eaab0aaf23c81e21dd850394f8
SIVARAMAKRISHNA <51481385+ullagallu123@users.noreply.github.com>
2024-10-26 20:16:21 +0530

    correct the error

--- docker-machine.sh
+++ docker-machine.sh
@@ -4,7 +4,7 @@ aws ec2 run-instances \
   --instance-type t3a.large \
   --key-name dev \
   --user-data file://docker-installation.sh \
-  #--instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent}" \
-  --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}"
+  #--instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=one-time}" \
+  --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}" \
   --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=docker}]' \
   --block-device-mappings 'DeviceName=/dev/xvda,Ebs={VolumeSize=50,VolumeType=gp3,DeleteOnTermination=true}'
