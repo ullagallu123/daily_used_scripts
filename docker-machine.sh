241fc257db2aa7412f08c5514d37db50aa42c309
SIVARAMAKRISHNA <51481385+ullagallu123@users.noreply.github.com>
2024-10-29 15:39:52 +0530

    Update docker-machine.sh

--- docker-machine.sh
+++ docker-machine.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+HOSTED_ZONE_ID="Z08801502JQFVUXR02K9R"  
+RECORD_NAME="docker.konkas.tech"   
+
 AMI_ID=$(aws ec2 describe-images \
     --owners "amazon" \
     --filters "Name=name,Values=amzn3-ami-hvm-*-x86_64-gp3" \
@@ -12,11 +15,41 @@ if [ -z "$AMI_ID" ]; then
   exit 1
 fi
 
-aws ec2 run-instances \
+INSTANCE_ID=$(aws ec2 run-instances \
   --image-id "$AMI_ID" \
   --instance-type t3a.large \
   --key-name dev \
   --user-data file://docker-installation.sh \
   --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=one-time}" \
   --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=docker}]' \
-  --block-device-mappings 'DeviceName=/dev/xvda,Ebs={VolumeSize=50,VolumeType=gp3,DeleteOnTermination=true}'
+  --block-device-mappings 'DeviceName=/dev/xvda,Ebs={VolumeSize=50,VolumeType=gp3,DeleteOnTermination=true}' \
+  --query 'Instances[0].InstanceId' \
+  --output text)
+
+aws ec2 wait instance-running --instance-ids "$INSTANCE_ID"
+PUBLIC_IP=$(aws ec2 describe-instances \
+  --instance-ids "$INSTANCE_ID" \
+  --query 'Reservations[0].Instances[0].PublicIpAddress' \
+  --output text)
+
+if [ -z "$PUBLIC_IP" ]; then
+  echo "Error: Failed to retrieve the public IP address for instance $INSTANCE_ID."
+  exit 1
+fi
+
+
+aws route53 change-resource-record-sets \
+  --hosted-zone-id "$HOSTED_ZONE_ID" \
+  --change-batch "{
+      \"Changes\": [{
+          \"Action\": \"UPSERT\",
+          \"ResourceRecordSet\": {
+              \"Name\": \"$RECORD_NAME\",
+              \"Type\": \"A\",
+              \"TTL\": 1,
+              \"ResourceRecords\": [{\"Value\": \"$PUBLIC_IP\"}]
+          }
+      }]
+  }"
+
+echo "Instance launched with ID: $INSTANCE_ID and DNS record $RECORD_NAME updated to IP: $PUBLIC_IP"
