0ad78e6f51dba45032d5eb4a8cbba1990f461e4e
SIVARAMAKRISHNA <51481385+ullagallu123@users.noreply.github.com>
2024-10-29 15:34:48 +0530

    Update docker-machine.sh

--- docker-machine.sh
+++ docker-machine.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
+
+AMI_ID=$(aws ec2 describe-images \
+    --owners "amazon" \
+    --filters "Name=name,Values=amzn3-ami-hvm-*-x86_64-gp3" \
+              "Name=state,Values=available" \
+    --query "Images | sort_by(@, &CreationDate) | [-1].ImageId" \
+    --output text)
+
+if [ -z "$AMI_ID" ]; then
+  echo "Error: Failed to retrieve the latest Amazon Linux 3 AMI ID."
+  exit 1
+fi
+
 aws ec2 run-instances \
-  --image-id ami-0a4408457f9a03be3 \
+  --image-id "$AMI_ID" \
   --instance-type t3a.large \
   --key-name dev \
   --user-data file://docker-installation.sh \
