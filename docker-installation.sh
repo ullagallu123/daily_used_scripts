35ede879a717cc9687b9f11339b16ee6926787f3
SIVARAMAKRISHNA <51481385+ullagallu123@users.noreply.github.com>
2024-11-21 10:05:36 +0530

    Update docker-installation.sh

--- docker-installation.sh
+++ docker-installation.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-dnf install -y git docker tmux
+dnf install -y git docker tmux tree
 systemctl start docker
 usermod -aG docker ec2-user
 curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
